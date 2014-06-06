96565e7ee8461b7e6e7ab444423986d974095178
AndresQuintero <AndresQuintero@users.noreply.github.com>
2014-06-06 12:23:03 -0500

    Update SeleccionMultiple.cpp

--- Framework/SeleccionMultiple.cpp
+++ Framework/SeleccionMultiple.cpp
@@ -2,12 +2,21 @@
 #include "SeleccionMultiple.hpp"
 
 SeleccionMultiple::SeleccionMultiple() {
-	
+	SeleccionMultiple::cantidadItems=0;
 }
-SeleccionMultiple::~SeleccionMultiple() {
+SeleccionMultiple::virtual~SeleccionMultiple() {
+	delete(this);
 }
 void SeleccionMultiple::adicionarRespuesta(std::string item,
 		bool respuesta) {
 	SeleccionMultiple::item.push_back(item);
 	SeleccionMultiple::respuestas.push_back(respuesta);
+	SeleccionMultiple::cantidadItems=SeleccionMultiple::incrementarCantidadItems();
+}
+
+int SeleccionMultiple::getCantidadItems(){
+	return SeleccionMultiple::cantidadItems;
+}
+void SeleccionMultiple::incrementarCantidadItems(){
+	SeleccionMultiple::cantidadItems++;
 }
