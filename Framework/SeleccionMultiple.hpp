f973c2b3dca4644e3e1630576ca5272685dd3dbc
AndresQuintero <AndresQuintero@users.noreply.github.com>
2014-06-07 16:49:21 -0500

    Update SeleccionMultiple.hpp

--- Framework/SeleccionMultiple.hpp
+++ Framework/SeleccionMultiple.hpp
@@ -4,25 +4,19 @@
 
 #include <string>
 #include <vector>
+
+template <typename Opcion>
+
 class SeleccionMultiple:Pregunta {
 private:
-<<<<<<< HEAD
-	string item;
-	bool respuestas;
-=======
-	std::vector<std::string> item;
-	std::vector<bool> respuestas;
-	int cantidadTotalItems;
->>>>>>> 8ccbd9a285235654bbf51cea8feaa7b179b0a5eb
+	std::vector<Opcion> item;
 public:
 	SeleccionMultiple();
 	~SeleccionMultiple();
-	void adicionarRespueta(std::string item,bool respuesta);
-	void cambiarRespuesta(int posicionPregunta, bool respuesta);
+	void adicionarOpcion(Opciones opcion);
+	void cambiarBoolOpcion(int posicionPregunta, bool valorRespuesta);
 	std::string getItem(int posicionPregunta);
-	bool getValorItem(int posicionPregunta);
-	int getCantidadItems();
-	void incrementarCantidadItems();
+	bool getBoolItem(int posicionPregunta);
 
 };
 
