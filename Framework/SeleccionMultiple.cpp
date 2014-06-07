77ed4dc66e2caa9827167556c3a52f83e8431b9d
AndresQuintero <AndresQuintero@users.noreply.github.com>
2014-06-07 17:34:06 -0500

    Update SeleccionMultiple.cpp

--- Framework/SeleccionMultiple.cpp
+++ Framework/SeleccionMultiple.cpp
@@ -2,21 +2,41 @@
 #include "SeleccionMultiple.hpp"
 
 SeleccionMultiple::SeleccionMultiple() {
-	SeleccionMultiple::cantidadItems=0;
+	
 }
 SeleccionMultiple::virtual~SeleccionMultiple() {
 	delete(this);
 }
-void SeleccionMultiple::adicionarRespuesta(std::string item,
-		bool respuesta) {
-	SeleccionMultiple::item.push_back(item);
-	SeleccionMultiple::respuestas.push_back(respuesta);
-	SeleccionMultiple::cantidadItems=SeleccionMultiple::incrementarCantidadItems();
+
+	SeleccionMultiple();
+	~SeleccionMultiple();
+	void 
+	bool getBoolItem(int posicionPregunta);
+	
+void SeleccionMultiple::adicionarOpcionOpciones(Opciones opcion) {
+	SeleccionMultiple::opciones.push_back(opcion);
 }
 
-int SeleccionMultiple::getCantidadItems(){
-	return SeleccionMultiple::cantidadItems;
+void SeleccionMultiple::cambiarBoolOpciones(int posicionPregunta, bool valorRespuesta){
+	SeleccionMultiple::opciones[posicionPregunta]=this->cambiarBoolOpcion(valorRespuesta);
 }
-void SeleccionMultiple::incrementarCantidadItems(){
-	SeleccionMultiple::cantidadItems++;
+
+void SeleccionMultiple::cambiarItemOpciones(int posicionPregunta, std::string item) {
+	SeleccionMultiple::opciones[posicionPregunta]=this->cambiarItemOpcion(item);
 }
+
+void eliminarOpcionOpciones(int posicionPregunta){
+	SeleccionMultiple::opciones.erase(opciones.begin()+posicionPregunta);
+}
+std::string getItemOpciones(int posicionPregunta){
+	std::string item;
+	item = SeleccionMultiple::opciones[posicionPregunta].getItemOpcion();
+	return item;
+}
+
+bool getBoolOpciones(int posicionPregunta){
+	bool valor;
+	valor = SeleccionMultiple::opciones[posicionPregunta].getValorOpcion();
+	return valor;
+}
+
