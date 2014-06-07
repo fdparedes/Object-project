6d5f3f32082b38a46e1c0c23c34e7f824c875fb0
natab27 <natab27@gmail.com>
2014-06-07 18:07:08 -0500

    Class Opcion.hpp

---
+++ Framework/Opcion.hpp
@@ -0,0 +1,24 @@
+#ifndef OPCION_HPP_
+#define OPCION_HPP_
+
+#include <string>
+#include <vector>
+
+template <typename Opcion>
+
+class Opcion
+{
+private:
+	std::string item;
+	bool valor;
+public:
+	Opcion();
+	virtual ~Opcion();
+	void cambiarBoolOpcion(bool);
+	void cambiarItemOpcion(std::string);
+	std::string getItemOpcion();
+	bool getValorOpcion();
+
+};
+
+#endif /* OPCION_HPP_ */
