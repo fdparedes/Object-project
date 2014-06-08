2544023d057a7a87ce528dde5bb2781d496ea351
AndresQuintero <AndresQuintero@users.noreply.github.com>
2014-06-07 22:36:46 -0500

    Update Pregunta.cpp

--- Framework/Pregunta.cpp
+++ Framework/Pregunta.cpp
@@ -1,10 +1,24 @@
 
 #include "Pregunta.hpp"
 
+Pregunta::Pregunta(std::string texto,bool valor) {
+	Pregunta::pregunta = texto;
+	pregunta::tipoPregunta=new FalsoVerdadero(valor);
+		
+}
 Pregunta::Pregunta(std::string texto) {
 	Pregunta::pregunta = texto;
+	pregunta::tipoPregunta= new SeleccionMultiple();
+		
+}
+
+Pregunta::Pregunta(std::string texto,bool valor) {
+	Pregunta::pregunta = texto;
+	pregunta::tipoPregunta=FalsoVerdadero(valor);
+		
 }
 Pregunta::~Pregunta() {
 	delete(this);
 }
+
 
