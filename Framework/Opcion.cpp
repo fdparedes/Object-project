cda9330e1f2807e6ea43b6660da3a6319750f8a7
Franco Paredes <frankparedes08@gmail.com>
2014-06-08 19:30:33 -0500

    Update Opcion.cpp

--- Framework/Opcion.cpp
+++ Framework/Opcion.cpp
@@ -5,13 +5,15 @@ Opcion::Opcion(std::string item,bool valor) {
 	Opcion::item=item;
 	Opcion::valor=valor;
 }
+
 Opcion::~Opcion() {
 }
+
 void Opcion::cambiarBoolOpcion() {
-	Opcion::valor=!Opcion::valor;
+	Opcion::valor = !Opcion::valor;
 }
 void Opcion::cambiarItemOpcion(std::string item) {
-	Opcion::item=item;
+	Opcion::item = item;
 }
 std::string Opcion::getItemOpcion() {
 	return Opcion::item;
